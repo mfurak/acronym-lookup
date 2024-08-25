abd2b5654da1336bc4f50463c5205a469098007b
mfurak <marek.furak@lego.com>
2024-08-25 19:57:53 +0200

    add env parameters setup to config

--- src/config.rs
+++ src/config.rs
@@ -7,3 +7,42 @@ pub struct CliParameters {
     #[arg(short, long, value_enum)]
     pub format: Option<OutputStyle>,
 }
+
+pub struct ConfluenceEnvParameters {
+    pub user_name: String,
+    pub api_token: String,
+    pub base_url: String,
+    pub page_id: String,
+}
+
+pub struct EnvParameters {
+    pub confluence: ConfluenceEnvParameters,
+    pub file_paths: Option<String>,
+}
+
+impl EnvParameters {
+    pub fn load() -> Self {
+        EnvParameters {
+            confluence: ConfluenceEnvParameters {
+                user_name: std::env::var("AL_CONFLUENCE_USER_NAME")
+                    .expect("AL_CONFLUENCE_USER_NAME is not set"),
+                api_token: std::env::var("AL_CONFLUENCE_API_TOKEN")
+                    .expect("AL_CONFLUENCE_API_TOKEN is not set"),
+                base_url: std::env::var("AL_CONFLUENCE_BASE_URL")
+                    .expect("AL_CONFLUENCE_BASE_URL is not set"),
+                page_id: std::env::var("AL_CONFLUENCE_PAGE_ID")
+                    .expect("AL_CONFLUENCE_PAGE_ID is not set"),
+            },
+            file_paths: match std::env::var("AL_FILE_PATHS") {
+                Ok(file_paths) => {
+                    if !file_paths.trim().is_empty() {
+                        Some(file_paths)
+                    } else {
+                        None
+                    }
+                }
+                Err(_) => None,
+            },
+        }
+    }
+}
